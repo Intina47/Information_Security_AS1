edd32e694cec91a0d59876f590894195591eb779
lujamitch <lujamitch@gmail.com>
2022-10-25 22:45:52 +0100

    added basic functionality for insecure need to add hashing in lab with openssl

--- insecure_login/login.cpp
+++ insecure_login/login.cpp
@@ -1,9 +1,168 @@
 #include <iostream>
+#include <fstream> 
+#include <string.h>
+#include <openssl/sha.h>
 #include "authlib.h"
+using namespace std;
 
 int main() {
+  
+  int choice;
+  cout << "Welcome to the Subverted Login System!" << endl << "Please select a choice from the following options" << endl << "*******************************************" << endl << "1. Register\n2. Login\n3. Forgot Credentials\n4. Exit\n  => " << endl;
+  cin >> choice;
+
+  switch (choice)
+  {
+  case 1:
+    register_user();
+    break;
+
+  case 2: 
+    login_user();
+    break; 
+
+  case 3: 
+    forgot_credentials();
+    break; 
+
+  case 4: 
+    exit(0);
+    break;
+  
+  default:
+    cout << "Please enter a valid choice!" << endl;
+    break;
+  }
+
   bool auth = true;
  
   if (auth) authenticated("user");
   else rejected("user");
+
+
+  return 0;
+}
+
+//register a user using a while loop until the user enters the same password twice
+void register_user()
+{
+    char username[20], password1[20], password2[20];
+    cout << "Enter Username: ";
+    cin >> username;
+    cout << "Enter Password: ";
+    cin >> password1;
+    cout << "Re-enter Password: ";
+    cin >> password2;
+
+    
+    while (strcmp(password1, password2) != 0)
+    {
+        cout << "\nPasswords do not match" << endl;
+        cout << "enter password:";
+        cin.getline(password1,20);
+
+        cout<< "Enter Password again:";
+        cin.getline(password2,20);
+    }
+    ofstream f_out;
+    f_out.open("passwords.txt", ios::app);
+    f_out << username << " " << password1 << endl;
+    f_out.close();
+    cout << "\nRegistration Successful\n" << endl;
+}
+void login_user()
+{
+    char username[20], password[20];
+    cout << "enter username: ";
+    cin >> username;
+    cout << "enter password: ";
+    cin >> password;
+
+
+    ifstream f_in;
+    f_in.open("passwords.txt");
+    char user[20], pass[20];
+    while (!f_in.eof())
+    {
+        f_in >> user >> pass;
+        if (strcmp(user, username) == 0 && strcmp(pass, password) == 0)
+        {
+            cout << "\nLogin Successful\n" << endl;
+            break;
+        }
+    }
+    if (f_in.eof())
+    {
+        cout << "\nInvalid Credentials\n" << endl;
+    }
+    f_in.close();
+}
+//when user selects forgot credentials, the user is asked to enter the username or password to reset
+void forgot_credentials()
+{
+    int choice;
+    cout << "1. Forgot Username\n2. Forgot Password\nKindly make a Choice to proceed: ";
+    cin >> choice;
+
+    switch (choice)
+    {
+        case 1: 
+            forgot_username();
+            break;
+    
+        case 2: 
+            forgot_password();
+            break;
+
+        default:
+            cout << "\nInvalid Choice\n" << endl;
+       
+    }
+}
+void forgot_username()
+{
+    char password[20];
+    cout << "enter password: ";
+    cin >> password;
+    //search for the password in the file
+    ifstream f_in;
+    f_in.open("passwords.txt");
+    char user[20], pass[20];
+    while (!f_in.eof())
+    {
+        f_in >> user >> pass;
+        if (strcmp(pass, password) == 0)
+        {
+            cout << "\nYour username is: " << user << endl;
+            break;
+        }
+    }
+    if (f_in.eof())
+    {
+        cout << "\nInvalid Credentials\n" << endl;
+    }
+}
+
+void forgot_password()
+{
+    char username[20];
+    cout << "enter username: ";
+    cin >> username;
+    //search for the username in the file
+    ifstream f_in;
+    f_in.open("passwords.txt");
+    char user[20], pass[20];
+    while (!f_in.eof())
+    {
+        f_in >> user >> pass;
+        if (strcmp(user, username) == 0)
+        {
+            cout << "\nYour password is: " << pass << endl;
+            break;
+        }
+    }
+    if (f_in.eof())
+    {
+        cout << "\nInvalid Credentials\n" << endl;
+    }
 }
