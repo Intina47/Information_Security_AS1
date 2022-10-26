346e2a16588acc3028206007392c3dbdc9907150
lujamitch <94070284+lujamitch@users.noreply.github.com>
2022-10-26 16:36:23 +0100

    updated just needs backend

--- insecure_login/login.cpp
+++ insecure_login/login.cpp
@@ -3,12 +3,22 @@
 #include <string.h>
 #include <openssl/sha.h>
 #include "authlib.h"
+
+
+void register_user();
+void login_user();
+void forgot_credentials();
+void forgot_password();
+void forgot_username();
+
+
 using namespace std;
+
 
 int main() {
   
   int choice;
-  cout << "Welcome to the Subverted Login System!" << endl << "Please select a choice from the following options" << endl << "*******************************************" << endl << "1. Register\n2. Login\n3. Forgot Credentials\n4. Exit\n  => " << endl;
+  cout << "Welcome to the Subverted Login System!" << endl << "Please select a choice from the following options" << endl << "*******************************************" << endl << "1. Register\n2. Login\n3. Exit\n  => " << endl;
   cin >> choice;
 
   switch (choice)
@@ -22,10 +32,6 @@ int main() {
     break; 
 
   case 3: 
-    forgot_credentials();
-    break; 
-
-  case 4: 
     exit(0);
     break;
   
@@ -34,19 +40,13 @@ int main() {
     break;
   }
 
-  bool auth = true;
- 
-  if (auth) authenticated("user");
-  else rejected("user");
-
-
   return 0;
 }
 
 //register a user using a while loop until the user enters the same password twice
 void register_user()
 {
-    char username[20], password1[20], password2[20];
+    string username, password1, password2;
     cout << "Enter Username: ";
     cin >> username;
     cout << "Enter Password: ";
@@ -55,114 +55,58 @@ void register_user()
     cin >> password2;
 
     
-    while (strcmp(password1, password2) != 0)
+    while (password1 != password2)
     {
         cout << "\nPasswords do not match" << endl;
         cout << "enter password:";
-        cin.getline(password1,20);
+
+        cin >> password1;
 
         cout<< "Enter Password again:";
-        cin.getline(password2,20);
+
+        cin >> password2;
     }
     ofstream f_out;
     f_out.open("passwords.txt", ios::app);
-    f_out << username << " " << password1 << endl;
+    f_out << username << ":" << password1 << endl;
     f_out.close();
     cout << "\nRegistration Successful\n" << endl;
 }
 void login_user()
 {
-    char username[20], password[20];
+    string username, password;
     cout << "enter username: ";
     cin >> username;
     cout << "enter password: ";
     cin >> password;
 
-
     ifstream f_in;
     f_in.open("passwords.txt");
-    char user[20], pass[20];
+    string user, pass;
     while (!f_in.eof())
     {
-        f_in >> user >> pass;
-        if (strcmp(user, username) == 0 && strcmp(pass, password) == 0)
+        getline(f_in, user, ':'); 
+
+        getline(f_in, pass);
+        
+        if (user == username && pass == password)
         {
             cout << "\nLogin Successful\n" << endl;
+            
+            f_in.close();
+
+            authenticated(user);
+            
             break;
         }
     }
-    if (f_in.eof())
-    {
-        cout << "\nInvalid Credentials\n" << endl;
-    }
+
+        if (f_in.eof())
+        {
+            cout << "\nInvalid Credentials\n" << endl;
+
+            rejected(user);
+        }
+
     f_in.close();
-}
-//when user selects forgot credentials, the user is asked to enter the username or password to reset
-void forgot_credentials()
-{
-    int choice;
-    cout << "1. Forgot Username\n2. Forgot Password\nKindly make a Choice to proceed: ";
-    cin >> choice;
-
-    switch (choice)
-    {
-        case 1: 
-            forgot_username();
-            break;
-    
-        case 2: 
-            forgot_password();
-            break;
-
-        default:
-            cout << "\nInvalid Choice\n" << endl;
-       
-    }
-}
-void forgot_username()
-{
-    char password[20];
-    cout << "enter password: ";
-    cin >> password;
-    //search for the password in the file
-    ifstream f_in;
-    f_in.open("passwords.txt");
-    char user[20], pass[20];
-    while (!f_in.eof())
-    {
-        f_in >> user >> pass;
-        if (strcmp(pass, password) == 0)
-        {
-            cout << "\nYour username is: " << user << endl;
-            break;
-        }
-    }
-    if (f_in.eof())
-    {
-        cout << "\nInvalid Credentials\n" << endl;
-    }
-}
-
-void forgot_password()
-{
-    char username[20];
-    cout << "enter username: ";
-    cin >> username;
-    //search for the username in the file
-    ifstream f_in;
-    f_in.open("passwords.txt");
-    char user[20], pass[20];
-    while (!f_in.eof())
-    {
-        f_in >> user >> pass;
-        if (strcmp(user, username) == 0)
-        {
-            cout << "\nYour password is: " << pass << endl;
-            break;
-        }
-    }
-    if (f_in.eof())
-    {
-        cout << "\nInvalid Credentials\n" << endl;
-    }
 }
